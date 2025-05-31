12812ff9f440e9ffe24fcb3c5fa015ea6431c103
Penguin-SAMA <496107146@qq.com>
2025-06-01 00:40:03 +0800

    update

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin",
+    flavour = "latte",
+    opts = {
+      transparent_background = true,
+    },
+  },
+}
