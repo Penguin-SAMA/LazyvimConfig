0a176759a09fba7eebe37761db85d3abb2e3947a
Penguin-SAMA <496107146@qq.com>
2026-03-23 07:58:08 +0800

    feat(snacks): add filename copy action

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,6 +10,22 @@ return {
               min_width = 27,
             },
           },
+          win = {
+            list = {
+              keys = {
+                ["n"] = { "yank_filename", desc = "Copy filename" },
+              },
+            },
+          },
+          actions = {
+            yank_filename = function(_, item)
+              if item then
+                local name = vim.fn.fnamemodify(item.file, ":t")
+                vim.fn.setreg("+", name)
+                Snacks.notify("Copied: " .. name)
+              end
+            end,
+          },
         },
       },
     },
